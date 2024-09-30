75de6c5e534755b742ef2062166c33fa1b182003
GiorgiGogaladze <gogaladzegio55@gmail.com>
2024-10-01 00:11:51 +0400

    creating page-not-found component and adding to the router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import AboutUs from './Components/AboutUs/AboutUs';
 import Contact from './Components/Contact/Contact';
+import PageNotFound from './Components/PageNotFound/PageNotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/AboutUs" element={<AboutUs />}/>
         <Route path="/Contact" element={<Contact />}/>
+        <Route path="*" element={<PageNotFound />}/>
       </Routes>
       <Footer />   
     </div>
