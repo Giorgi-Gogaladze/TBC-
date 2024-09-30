24f6d2fa388b424f76405e47dffd12303148cb08
GiorgiGogaladze <gogaladzegio55@gmail.com>
2024-10-01 01:22:58 +0400

    creating `Contact Us` page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Contact.css'
+import contactUs from '../../assets/contact-us-image.jpg'
 
 function Contact() {
   return (
-    <div>
-    </div>
+    <section className='contact main-width'>
+      <div className='half'>
+        <img src={contactUs} alt="contactUs" />
+      </div>
+      <div className='half'>
+        <form >
+        <h1>Contact Us</h1>
+          <input type="text" name="name"  placeholder='Name...'/>
+          <input type="email" name="emali" placeholder='Emali...' />
+          <textarea type="text" name="mes" placeholder='Message...'/>
+          <button>contact</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
