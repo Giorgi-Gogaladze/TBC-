395a768fc4c49d5c84566dda099d3d30aefd264a
GiorgiGogaladze <gogaladzegio55@gmail.com>
2024-10-03 15:28:46 +0400

    compliting Profile page, adding form inputs

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Profile.css'
+import Norton from '../../assets/Norton.jpg'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <article className='main-width profile'>
+        <div className='whole-card'>
+            <div className='norton'>
+                <img src={Norton} alt="Edward-Norton" />
+            </div>
+            <div className='information'>
+                <form>
+                    <label >Name:</label>
+                    <input type="text" name="name" value='Edward' />
+                    <label >LastName:</label>
+                    <input type="text" name="lastName" value='Norton' />
+                    <label >Email:</label>
+                    <input type="email" name="email" value=' gogaladze.giorgi@tbcacademy.edu.ge' />
+                </form>
+                
+            </div>
+        </div>
+    </article>
   )
 }
 
