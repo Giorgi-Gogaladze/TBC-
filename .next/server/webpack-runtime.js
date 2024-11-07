fe3bbf12b07120270bcedf4ec3a5abf7c78f95a5
GiorgiGogaladze <gogaladzegio55@gmail.com>
2024-11-08 00:01:44 +0400

    The trial of creating internationaliation using i18next

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c65947b784d45c67")
+/******/ 		__webpack_require__.h = () => ("e945df87cf150717")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ceffb9ba14e8333"; }
+/******/ 		__webpack_require__.h = function() { return "d3007ae8cc47658a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
